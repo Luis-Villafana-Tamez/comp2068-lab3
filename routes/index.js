a7dbd3a318ec77450f9234d7c30506e6c1000d85
Chris Naismith <christophernaismith@gmail.com>
2019-05-28 19:16:22 -0400

    Creation of routes / creation of chris.pug

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Example' });
 });
 
+router.get('/chris', (req, res, next) => {
+  res.render('chris', {});
+});
+
+router.get('/rebecca', (req, res, next) => {
+  res.render('rebecca', {});
+});
+
+router.get('/andrew', (req, res, next) => {
+  res.render('andrew', {});
+});
+
+router.get('/dean', (req, res, next) => {
+  res.render('dean', {});
+});
+
+router.get('/cindy', (req, res, next) => {
+  res.render('cindy', {});
+});
+
 module.exports = router;
