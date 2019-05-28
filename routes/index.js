c03f24ff55e7d9c7c6ec3079ea2e82db9d47ea15
Chris Naismith <christophernaismith@gmail.com>
2019-05-28 19:37:50 -0400

    Using vars in index route

--- routes/index.js
+++ routes/index.js
@@ -7,23 +7,57 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/chris', (req, res, next) => {
-  res.render('chris', {});
+  res.render('chris', {
+    age: 24,
+    cityResiding: 'Stayner',
+    cityBorn: 'Barrie',
+    likes: 'playing Rocket League',
+    dislikes: 'loud people on the bus and subway'
+  });
 });
 
 router.get('/rebecca', (req, res, next) => {
-  res.render('rebecca', {});
+  res.render('rebecca', {
+    age: 27,
+    cityResiding: 'Stanyer',
+    cityBorn: 'Hanover',
+    likes: 'pizza',
+    dislikes: 'doing report cards',
+    relation: 'wife'
+  });
 });
 
 router.get('/andrew', (req, res, next) => {
-  res.render('andrew', {});
+  res.render('andrew', {
+    age: 22,
+    cityResiding: 'Barrie',
+    cityBorn: 'Barrie',
+    likes: 'using PS4',
+    dislikes: 'vegetables',
+    relation: 'brother'
+  });
 });
 
 router.get('/dean', (req, res, next) => {
-  res.render('dean', {});
+  res.render('dean', {
+    age: 53,
+    cityResiding: 'Barrie',
+    cityBorn: 'Barrie',
+    likes: 'beer',
+    dislikes: 'not beer',
+    relation: 'father'
+  });
 });
 
 router.get('/cindy', (req, res, next) => {
-  res.render('cindy', {});
+  res.render('cindy', {
+    age: 56,
+    cityResiding: 'Barrie',
+    cityBorn: 'Barrie',
+    likes: 'the computer',
+    dislikes: 'beer',
+    relation: 'mom'
+  });
 });
 
 module.exports = router;
